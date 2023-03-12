b88afe3a87251f05ba4e2a71db457f2d9fc17e87
AnaeJ <19928788-AnaeJ@users.noreply.replit.com>
2023-03-12 09:46:16 +0000

    First commit

--- index.js
+++ index.js
@@ -23,10 +23,11 @@
 /**********************************************************
  * @param {1} Import_Modules for this FIle
  *********************************************************/
-const Discord = require("discord.js");
+const { Client, GatewayIntentBits } = require('discord.js');
 const colors = require("colors");
 const enmap = require("enmap");
 const fs = require("fs");
+const express = require("./server.js");
 const emojis = require("./botconfig/emojis.json");
 const config = require(`./botconfig/config.json`);
 const advertisement = require("./botconfig/advertisement.json");
@@ -40,7 +41,7 @@ require('dotenv').config();
 /**********************************************************
  * @param {2} CREATE_THE_DISCORD_BOT_CLIENT with some default settings
  *********************************************************/
-const client = new Discord.Client({
+const client = new Client({
   fetchAllMembers: false,
   restTimeOffset: 0,
   failIfNotExists: false,
@@ -50,21 +51,21 @@ const client = new Discord.Client({
     repliedUser: false,
   },
   partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
-  intents: [Discord.Intents.FLAGS.GUILDS,
-    Discord.Intents.FLAGS.GUILD_MEMBERS,
-    Discord.Intents.FLAGS.GUILD_BANS,
-    Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-    Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
-    Discord.Intents.FLAGS.GUILD_WEBHOOKS,
-    Discord.Intents.FLAGS.GUILD_INVITES,
-    Discord.Intents.FLAGS.GUILD_VOICE_STATES,
-    Discord.Intents.FLAGS.GUILD_PRESENCES,
-    Discord.Intents.FLAGS.GUILD_MESSAGES,
-    Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    //Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
-    Discord.Intents.FLAGS.DIRECT_MESSAGES,
-    Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-    //Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING
+  intents: [GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildWebhooks,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessageTyping
   ],
   presence: {
     activities: [{
